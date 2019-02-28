63947d6d1fe7dbd666e62e024ce5989da10daa0d
hsimao <e087754958@gmail.com>
2019-02-28 15:47:52 +0800

    add: 新增action、reducers, 將router移回根目錄index.js 統一跟redux一同設置

--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
 
+// router components
+import Register from './components/Auth/Register'
+import App from './components/App'
+import Login from './components/Auth/Login'
+
+import firebase from './firebase'
+
 import 'semantic-ui-css/semantic.min.css'
-import RootWithAuth from './router'
 
 // redux 設置
 import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-const store = createStore(() => {}, composeWithDevTools())
+import rootReducer from './reducers'
+import { setUser } from './actions'
+const store = createStore(rootReducer, composeWithDevTools())
+
+// router 設置
+class Root extends Component {
+  componentDidMount() {
+    // 監聽 onAuthStateChanged, 如果有取得 user 資料就引導到首頁
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log('user', user)
+        // 將 user 資料儲存到 redux
+        this.props.setUser(user)
+        this.props.history.push('/')
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    )
+  }
+}
+
+// router 連結 redux
+const RootWithAuth = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
+)
 
 ReactDOM.render(
   <Provider store={store}>
